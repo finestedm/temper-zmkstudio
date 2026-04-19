602a1c7dbaaea193c784211c51f1a62c005caf0a
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2026-04-19 13:23:13 +0200

    Add studio_unlock.h include to custom_config.h

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -1,6 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#include <dt-bindings/zmk/studio_unlock.h>
+
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_EXTRA_COLEMAKDH
 #define MIRYOKU_NAV_VI
