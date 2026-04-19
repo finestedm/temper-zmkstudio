bfb7b6ff1e4ff1c0e0c84edd0b573d8a3ead95bf
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2026-04-19 12:52:07 +0200

    Add media layer configuration with studio unlock
    Added a media layer configuration with studio unlock functionality.

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -4,3 +4,10 @@
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_EXTRA_COLEMAKDH
 #define MIRYOKU_NAV_VI
+
+// Add this to put Studio Unlock on the Media Layer (Top Left Key)
+#define MIRYOKU_LAYER_MEDIA \
+&studio_unlock,    &kp VOLD,          &kp VOLU,          &kp MUTE,          &trans,            &trans,            &kp STOP,          &kp PREV,          &kp NEXT,          &kp PP,            \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &trans,            &trans,            &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&trans,            &kp ALGR,          &trans,            &trans,            &trans,            &trans,            &trans,            &trans,            &kp ALGR,          &trans,            \
+&trans,            &trans,            &trans,            &kp PP,            &kp NEXT,          &kp PREV
