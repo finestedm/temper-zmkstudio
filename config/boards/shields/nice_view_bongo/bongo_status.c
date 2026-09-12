f17197b4cc5b9aa4b4d55d74940f629a3b3b2c1f
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2026-09-12 12:40:33 +0200

    feat: rotate and refine Bongo display

--- config/boards/shields/nice_view_bongo/bongo_status.c
+++ config/boards/shields/nice_view_bongo/bongo_status.c
@@ -6,11 +6,18 @@
 #include <zephyr/logging/log.h>
 
 #include <zmk/battery.h>
+#include <zmk/ble.h>
 #include <zmk/display.h>
+#include <zmk/endpoints.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/battery_state_changed.h>
+#include <zmk/events/ble_active_profile_changed.h>
+#include <zmk/events/endpoint_changed.h>
 #include <zmk/events/keycode_state_changed.h>
+#include <zmk/events/layer_state_changed.h>
+#include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/events/wpm_state_changed.h>
+#include <zmk/keymap.h>
 #include <zmk/wpm.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -20,16 +27,26 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
+struct bongo_key_state {
+    bool pressed;
+};
+
 struct bongo_battery_state {
     uint8_t level;
 };
 
-struct bongo_wpm_state {
-    uint8_t wpm;
+struct bongo_connection_state {
+    enum bongo_connection_status status;
+    uint8_t profile_index;
 };
 
-struct bongo_key_state {
-    bool pressed;
+struct bongo_layer_state {
+    uint8_t index;
+    const char *label;
+};
+
+struct bongo_wpm_state {
+    uint8_t wpm;
 };
 
 static void init_rect(lv_draw_rect_dsc_t *dsc, lv_color_t fill, int border_width,
@@ -50,11 +67,10 @@ static void init_line(lv_draw_line_dsc_t *dsc, int width) {
     dsc->round_end = true;
 }
 
-static void init_label(lv_draw_label_dsc_t *dsc, const lv_font_t *font,
-                       lv_text_align_t align) {
+static void init_label(lv_draw_label_dsc_t *dsc, lv_text_align_t align) {
     lv_draw_label_dsc_init(dsc);
     dsc->color = BONGO_FOREGROUND;
-    dsc->font = font;
+    dsc->font = &lv_font_unscii_8;
     dsc->align = align;
 }
 
@@ -88,107 +104,151 @@ static void draw_text(lv_obj_t *canvas, int x, int y, int width, lv_draw_label_d
     lv_layer_t layer;
     lv_canvas_init_layer(canvas, &layer);
     dsc->text = text;
-    lv_area_t coords = {x, y, x + width - 1, BONGO_HEIGHT - 1};
+    lv_area_t coords = {x, y, x + width - 1, BONGO_LOGICAL_HEIGHT - 1};
     lv_draw_label(&layer, dsc, &coords);
     lv_canvas_finish_layer(canvas, &layer);
 }
 
 static void draw_battery(lv_obj_t *canvas, uint8_t level) {
-    lv_draw_rect_dsc_t outline;
-    lv_draw_rect_dsc_t empty;
-    lv_draw_rect_dsc_t fill;
-
-    init_rect(&outline, BONGO_FOREGROUND, 0, 0);
-    init_rect(&empty, BONGO_BACKGROUND, 0, 0);
-    init_rect(&fill, BONGO_FOREGROUND, 0, 0);
-
-    draw_rect(canvas, 3, 4, 27, 11, &outline);
-    draw_rect(canvas, 5, 6, 23, 7, &empty);
-    draw_rect(canvas, 30, 7, 3, 5, &outline);
-
-    int fill_width = (level * 21 + 99) / 100;
-    if (fill_width > 0) {
-        draw_rect(canvas, 6, 7, fill_width, 5, &fill);
-    }
-}
-
-static void draw_cat(lv_obj_t *canvas, bool alternate_paw) {
-    lv_draw_rect_dsc_t outline;
     lv_draw_rect_dsc_t black;
-    lv_draw_line_dsc_t thick_line;
-    lv_draw_line_dsc_t thin_line;
+    lv_draw_rect_dsc_t white;
 
-    init_rect(&outline, BONGO_BACKGROUND, 2, 8);
-    init_rect(&black, BONGO_FOREGROUND, 0, 1);
-    init_line(&thick_line, 3);
-    init_line(&thin_line, 2);
+    init_rect(&black, BONGO_FOREGROUND, 0, 0);
+    init_rect(&white, BONGO_BACKGROUND, 0, 0);
 
-    const lv_point_t left_ear[] = {{51, 18}, {56, 7}, {68, 16}};
-    const lv_point_t right_ear[] = {{96, 16}, {106, 7}, {111, 19}};
-    draw_line(canvas, left_ear, 3, &thick_line);
-    draw_line(canvas, right_ear, 3, &thick_line);
+    draw_rect(canvas, 2, 3, 18, 9, &black);
+    draw_rect(canvas, 4, 5, 14, 5, &white);
+    draw_rect(canvas, 20, 5, 2, 5, &black);
 
-    draw_rect(canvas, 50, 14, 63, 39, &outline);
-
-    draw_rect(canvas, 65, 27, 4, 6, &black);
-    draw_rect(canvas, 94, 27, 4, 6, &black);
-
-    const lv_point_t mouth_left[] = {{76, 37}, {81, 41}, {86, 37}};
-    const lv_point_t mouth_right[] = {{86, 37}, {91, 41}, {96, 37}};
-    draw_line(canvas, mouth_left, 3, &thin_line);
-    draw_line(canvas, mouth_right, 3, &thin_line);
-
-    const lv_point_t whisker_left_top[] = {{56, 36}, {43, 33}};
-    const lv_point_t whisker_left_bottom[] = {{56, 41}, {42, 44}};
-    const lv_point_t whisker_right_top[] = {{107, 36}, {120, 33}};
-    const lv_point_t whisker_right_bottom[] = {{107, 41}, {121, 44}};
-    draw_line(canvas, whisker_left_top, 2, &thin_line);
-    draw_line(canvas, whisker_left_bottom, 2, &thin_line);
-    draw_line(canvas, whisker_right_top, 2, &thin_line);
-    draw_line(canvas, whisker_right_bottom, 2, &thin_line);
-
-    draw_rect(canvas, 43, 54, 82, 12, &outline);
-    for (int x = 49; x < 120; x += 9) {
-        draw_rect(canvas, x, 59, 5, 2, &black);
+    int fill_width = (level * 12 + 99) / 100;
+    if (fill_width > 0) {
+        draw_rect(canvas, 5, 6, fill_width, 3, &black);
     }
-
-    int left_paw_y = alternate_paw ? 48 : 52;
-    int right_paw_y = alternate_paw ? 52 : 48;
-    draw_rect(canvas, 59, left_paw_y, 19, 10, &outline);
-    draw_rect(canvas, 88, right_paw_y, 19, 10, &outline);
 }
 
-static void draw_status(struct zmk_widget_bongo_status *widget) {
-    lv_draw_label_dsc_t small_left;
-    lv_draw_label_dsc_t small_right;
-    lv_draw_label_dsc_t title;
-    char battery_text[8];
-    char wpm_text[12];
+static void draw_indicators(lv_obj_t *canvas, const struct zmk_widget_bongo_status *widget) {
+    lv_draw_label_dsc_t left;
+    lv_draw_label_dsc_t right;
+    char battery_text[6];
+    char connection_text[6];
+    char layer_text[8];
+    char wpm_text[8];
 
-    init_label(&small_left, &lv_font_unscii_8, LV_TEXT_ALIGN_LEFT);
-    init_label(&small_right, &lv_font_unscii_8, LV_TEXT_ALIGN_RIGHT);
-    init_label(&title, &lv_font_montserrat_14, LV_TEXT_ALIGN_CENTER);
-
-    lv_canvas_fill_bg(widget->canvas, BONGO_BACKGROUND, LV_OPA_COVER);
-    draw_battery(widget->canvas, widget->battery);
-    draw_cat(widget->canvas, widget->alternate_paw);
+    init_label(&left, LV_TEXT_ALIGN_LEFT);
+    init_label(&right, LV_TEXT_ALIGN_RIGHT);
 
     snprintf(battery_text, sizeof(battery_text), "%u%%", widget->battery);
+    switch (widget->connection) {
+    case BONGO_CONNECTION_USB:
+        strcpy(connection_text, "USB");
+        break;
+    case BONGO_CONNECTION_BLE_CONNECTED:
+        snprintf(connection_text, sizeof(connection_text), "B%u+", widget->profile_index);
+        break;
+    case BONGO_CONNECTION_BLE_DISCONNECTED:
+        snprintf(connection_text, sizeof(connection_text), "B%u-", widget->profile_index);
+        break;
+    default:
+        snprintf(connection_text, sizeof(connection_text), "B%u?", widget->profile_index);
+        break;
+    }
+
+    if (widget->layer_label != NULL && strlen(widget->layer_label) > 0) {
+        snprintf(layer_text, sizeof(layer_text), "L %.4s", widget->layer_label);
+    } else {
+        snprintf(layer_text, sizeof(layer_text), "L%u", widget->layer_index);
+    }
     snprintf(wpm_text, sizeof(wpm_text), "%u WPM", widget->wpm);
-    draw_text(widget->canvas, 2, 17, 34, &small_left, battery_text);
-    draw_text(widget->canvas, 126, 5, 32, &small_right, wpm_text);
-    draw_text(widget->canvas, 124, 24, 35, &title, "BONGO");
+
+    draw_battery(canvas, widget->battery);
+    draw_text(canvas, 25, 1, 22, &left, battery_text);
+    draw_text(canvas, 48, 1, 18, &right, connection_text);
+    draw_text(canvas, 2, 15, 30, &left, layer_text);
+    draw_text(canvas, 33, 15, 33, &right, wpm_text);
 }
 
-static void set_battery_status(struct zmk_widget_bongo_status *widget,
-                               struct bongo_battery_state state) {
-    widget->battery = state.level;
-    draw_status(widget);
+static void draw_paw(lv_obj_t *canvas, int x, bool tapping) {
+    lv_draw_rect_dsc_t outline;
+    lv_draw_rect_dsc_t black;
+    lv_draw_line_dsc_t motion_line;
+
+    init_rect(&outline, BONGO_BACKGROUND, 2, 9);
+    init_rect(&black, BONGO_FOREGROUND, 0, 3);
+    init_line(&motion_line, 2);
+
+    if (tapping) {
+        /* The long paw is the one currently hitting the desk. */
+        draw_rect(canvas, x, 98, 20, 47, &outline);
+
+        const lv_point_t left_tap[] = {{x + 3, 150}, {x, 155}};
+        const lv_point_t middle_tap[] = {{x + 10, 151}, {x + 10, 157}};
+        const lv_point_t right_tap[] = {{x + 17, 150}, {x + 20, 155}};
+        draw_line(canvas, left_tap, 2, &motion_line);
+        draw_line(canvas, middle_tap, 2, &motion_line);
+        draw_line(canvas, right_tap, 2, &motion_line);
+    } else {
+        /* The raised paw shows the simple monochrome paw pads from Bongo Cat. */
+        draw_rect(canvas, x, 99, 20, 32, &outline);
+        draw_rect(canvas, x + 7, 114, 6, 8, &black);
+        draw_rect(canvas, x + 3, 108, 4, 4, &black);
+        draw_rect(canvas, x + 8, 106, 4, 4, &black);
+        draw_rect(canvas, x + 13, 108, 4, 4, &black);
+    }
+}
+
+static void draw_bongo_cat(lv_obj_t *canvas, bool alternate_paw) {
+    lv_draw_rect_dsc_t black;
+    lv_draw_line_dsc_t outline;
+    lv_draw_line_dsc_t face;
+
+    init_rect(&black, BONGO_FOREGROUND, 0, 2);
+    init_line(&outline, 3);
+    init_line(&face, 2);
+
+    lv_canvas_fill_bg(canvas, BONGO_BACKGROUND, LV_OPA_COVER);
+
+    /* The characteristic Bongo Cat blob: peaked ears, arched back, and tail. */
+    const lv_point_t body[] = {
+        {4, 103}, {2, 94},  {3, 77},  {8, 62},  {16, 52}, {21, 33},
+        {29, 45}, {40, 47}, {51, 55}, {63, 49}, {64, 70}, {61, 82},
+        {66, 94}, {62, 103}, {55, 109}, {13, 109}, {4, 103},
+    };
+    draw_line(canvas, body, ARRAY_SIZE(body), &outline);
+
+    /* Dot eyes and the small W-shaped mouth from the original animation. */
+    draw_rect(canvas, 21, 70, 4, 6, &black);
+    draw_rect(canvas, 45, 70, 4, 6, &black);
+    const lv_point_t mouth[] = {{28, 80}, {31, 84}, {34, 80}, {37, 84}, {41, 80}};
+    draw_line(canvas, mouth, ARRAY_SIZE(mouth), &face);
+
+    draw_paw(canvas, 8, !alternate_paw);
+    draw_paw(canvas, 40, alternate_paw);
+}
+
+static void rotate_clockwise(struct zmk_widget_bongo_status *widget) {
+    const uint32_t source_stride =
+        lv_draw_buf_width_to_stride(BONGO_LOGICAL_WIDTH, BONGO_COLOR_FORMAT);
+    const uint32_t display_stride =
+        lv_draw_buf_width_to_stride(BONGO_DISPLAY_WIDTH, BONGO_COLOR_FORMAT);
+
+    lv_draw_sw_rotate(widget->drawing_buf, widget->display_buf, BONGO_LOGICAL_WIDTH,
+                      BONGO_LOGICAL_HEIGHT, source_stride, display_stride,
+                      LV_DISPLAY_ROTATION_90, BONGO_COLOR_FORMAT);
+    lv_obj_invalidate(widget->display_canvas);
+}
+
+static void draw_frame(struct zmk_widget_bongo_status *widget) {
+    draw_bongo_cat(widget->drawing_canvas, widget->alternate_paw);
+    draw_indicators(widget->drawing_canvas, widget);
+    rotate_clockwise(widget);
 }
 
 static void battery_update_cb(struct bongo_battery_state state) {
     struct zmk_widget_bongo_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_status(widget, state); }
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        widget->battery = state.level;
+        draw_frame(widget);
+    }
 }
 
 static struct bongo_battery_state battery_get_state(const zmk_event_t *eh) {
@@ -202,14 +262,72 @@ ZMK_DISPLAY_WIDGET_LISTENER(bongo_battery_listener, struct bongo_battery_state,
                             battery_update_cb, battery_get_state)
 ZMK_SUBSCRIPTION(bongo_battery_listener, zmk_battery_state_changed);
 
-static void set_wpm_status(struct zmk_widget_bongo_status *widget, struct bongo_wpm_state state) {
-    widget->wpm = state.wpm;
-    draw_status(widget);
+static void connection_update_cb(struct bongo_connection_state state) {
+    struct zmk_widget_bongo_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        widget->connection = state.status;
+        widget->profile_index = state.profile_index;
+        draw_frame(widget);
+    }
 }
+
+static struct bongo_connection_state connection_get_state(const zmk_event_t *eh) {
+    const struct zmk_endpoint_instance selected = zmk_endpoint_get_selected();
+    struct bongo_connection_state state = {
+        .status = BONGO_CONNECTION_USB,
+        .profile_index = zmk_ble_active_profile_index() + 1,
+    };
+
+    if (selected.transport == ZMK_TRANSPORT_BLE) {
+        if (zmk_ble_active_profile_is_connected()) {
+            state.status = BONGO_CONNECTION_BLE_CONNECTED;
+        } else if (zmk_ble_active_profile_is_open()) {
+            state.status = BONGO_CONNECTION_BLE_OPEN;
+        } else {
+            state.status = BONGO_CONNECTION_BLE_DISCONNECTED;
+        }
+    }
+
+    return state;
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(bongo_connection_listener, struct bongo_connection_state,
+                            connection_update_cb, connection_get_state)
+ZMK_SUBSCRIPTION(bongo_connection_listener, zmk_endpoint_changed);
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+ZMK_SUBSCRIPTION(bongo_connection_listener, zmk_usb_conn_state_changed);
+#endif
+#if IS_ENABLED(CONFIG_ZMK_BLE)
+ZMK_SUBSCRIPTION(bongo_connection_listener, zmk_ble_active_profile_changed);
+#endif
+
+static void layer_update_cb(struct bongo_layer_state state) {
+    struct zmk_widget_bongo_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        widget->layer_index = state.index;
+        widget->layer_label = state.label;
+        draw_frame(widget);
+    }
+}
+
+static struct bongo_layer_state layer_get_state(const zmk_event_t *eh) {
+    const zmk_keymap_layer_index_t index = zmk_keymap_highest_layer_active();
+    return (struct bongo_layer_state){
+        .index = index,
+        .label = zmk_keymap_layer_name(zmk_keymap_layer_index_to_id(index)),
+    };
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(bongo_layer_listener, struct bongo_layer_state, layer_update_cb,
+                            layer_get_state)
+ZMK_SUBSCRIPTION(bongo_layer_listener, zmk_layer_state_changed);
 
 static void wpm_update_cb(struct bongo_wpm_state state) {
     struct zmk_widget_bongo_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_wpm_status(widget, state); }
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        widget->wpm = state.wpm;
+        draw_frame(widget);
+    }
 }
 
 static struct bongo_wpm_state wpm_get_state(const zmk_event_t *eh) {
@@ -228,7 +346,7 @@ static void key_update_cb(struct bongo_key_state state) {
     struct zmk_widget_bongo_status *widget;
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
         widget->alternate_paw = !widget->alternate_paw;
-        draw_status(widget);
+        draw_frame(widget);
     }
 }
 
@@ -243,15 +361,23 @@ ZMK_SUBSCRIPTION(bongo_key_listener, zmk_keycode_state_changed);
 
 int zmk_widget_bongo_status_init(struct zmk_widget_bongo_status *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
-    lv_obj_set_size(widget->obj, BONGO_WIDTH, BONGO_HEIGHT);
+    lv_obj_set_size(widget->obj, BONGO_DISPLAY_WIDTH, BONGO_DISPLAY_HEIGHT);
 
-    widget->canvas = lv_canvas_create(widget->obj);
-    lv_obj_align(widget->canvas, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_canvas_set_buffer(widget->canvas, widget->cbuf, BONGO_WIDTH, BONGO_HEIGHT,
-                         BONGO_COLOR_FORMAT);
+    widget->drawing_canvas = lv_canvas_create(widget->obj);
+    lv_canvas_set_buffer(widget->drawing_canvas, widget->drawing_buf, BONGO_LOGICAL_WIDTH,
+                         BONGO_LOGICAL_HEIGHT, BONGO_COLOR_FORMAT);
+    lv_obj_add_flag(widget->drawing_canvas, LV_OBJ_FLAG_HIDDEN);
+
+    widget->display_canvas = lv_canvas_create(widget->obj);
+    lv_obj_align(widget->display_canvas, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_canvas_set_buffer(widget->display_canvas, widget->display_buf, BONGO_DISPLAY_WIDTH,
+                         BONGO_DISPLAY_HEIGHT, BONGO_COLOR_FORMAT);
 
     sys_slist_append(&widgets, &widget->node);
+    draw_frame(widget);
     bongo_battery_listener_init();
+    bongo_connection_listener_init();
+    bongo_layer_listener_init();
     bongo_wpm_listener_init();
     bongo_key_listener_init();
 
--- config/boards/shields/nice_view_bongo/bongo_status.h
+++ config/boards/shields/nice_view_bongo/bongo_status.h
@@ -3,20 +3,41 @@
 #include <lvgl.h>
 #include <zephyr/kernel.h>
 
-#define BONGO_WIDTH 160
-#define BONGO_HEIGHT 68
+/* Draw upright in the screen's physical 68x160 orientation, then rotate the
+ * finished frame clockwise into the controller's 160x68 framebuffer. */
+#define BONGO_LOGICAL_WIDTH 68
+#define BONGO_LOGICAL_HEIGHT 160
+#define BONGO_DISPLAY_WIDTH 160
+#define BONGO_DISPLAY_HEIGHT 68
 #define BONGO_COLOR_FORMAT LV_COLOR_FORMAT_L8
-#define BONGO_BUF_SIZE                                                                            \
-    LV_CANVAS_BUF_SIZE(BONGO_WIDTH, BONGO_HEIGHT, LV_COLOR_FORMAT_GET_BPP(BONGO_COLOR_FORMAT),    \
-                       LV_DRAW_BUF_STRIDE_ALIGN)
+
+#define BONGO_DRAW_BUF_SIZE                                                                       \
+    LV_CANVAS_BUF_SIZE(BONGO_LOGICAL_WIDTH, BONGO_LOGICAL_HEIGHT,                                 \
+                       LV_COLOR_FORMAT_GET_BPP(BONGO_COLOR_FORMAT), LV_DRAW_BUF_STRIDE_ALIGN)
+#define BONGO_DISPLAY_BUF_SIZE                                                                    \
+    LV_CANVAS_BUF_SIZE(BONGO_DISPLAY_WIDTH, BONGO_DISPLAY_HEIGHT,                                 \
+                       LV_COLOR_FORMAT_GET_BPP(BONGO_COLOR_FORMAT), LV_DRAW_BUF_STRIDE_ALIGN)
+
+enum bongo_connection_status {
+    BONGO_CONNECTION_USB,
+    BONGO_CONNECTION_BLE_CONNECTED,
+    BONGO_CONNECTION_BLE_DISCONNECTED,
+    BONGO_CONNECTION_BLE_OPEN,
+};
 
 struct zmk_widget_bongo_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_obj_t *canvas;
-    uint8_t cbuf[BONGO_BUF_SIZE];
+    lv_obj_t *drawing_canvas;
+    lv_obj_t *display_canvas;
+    uint8_t drawing_buf[BONGO_DRAW_BUF_SIZE];
+    uint8_t display_buf[BONGO_DISPLAY_BUF_SIZE];
     uint8_t battery;
     uint8_t wpm;
+    uint8_t layer_index;
+    uint8_t profile_index;
+    const char *layer_label;
+    enum bongo_connection_status connection;
     bool alternate_paw;
 };
 
